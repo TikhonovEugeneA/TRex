da322aca34cebd1af8b7d824c514bf72c9d4cba5
TikhonovEugeneA <tih050504@gmail.com>
2025-12-31 15:36:02 +0700

    Добавляет скрипт движения шаблонов карты

---
+++ Assets/Scripts/Map/MapMover.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+
+public class MapMover : MonoBehaviour
+{
+    [Header("Settings")] 
+    [Tooltip("Minimum moving speed.")]
+    [SerializeField] private float minSpeed;
+    
+    [Tooltip("Maximum moving speed.")]
+    [SerializeField] private float maxSpeed;    
+    [Tooltip("Current speed.")]
+    [SerializeField] private float speed;
+        
+    [Tooltip("Boost moving speed per second.")]
+    [SerializeField] private float boostSpeedPerSecond;
+
+    [SerializeField] private float nonBoostSpeedTime;
+        
+    [SerializeField] private bool isPLay;
+
+    private void Update()
+    {
+        if (this.isPLay)
+        {
+            Vector3 position = transform.position;
+            position = Vector3.Lerp(position, position + Vector3.left, Time.deltaTime * this.speed);
+            transform.position = position;
+        }
+    }
+
+    private IEnumerator SpeedCounter()
+    {
+        yield return new WaitForSeconds(this.nonBoostSpeedTime);
+    
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            this.speed += boostSpeedPerSecond / 10;
+
+            this.speed = Mathf.Clamp(this.speed, this.minSpeed, this.maxSpeed);
+        }
+    }
+
+    public void OnGameStart()
+    {
+        StartCoroutine(SpeedCounter());
+        this.isPLay = true;
+    }
+
+    public void OnPause() => this.isPLay = false;
+        
+    public void OnContinue() => this.isPLay = true;
+    }
