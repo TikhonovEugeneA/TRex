9bcf698f5bf269c38b1a160fad0591b8685bebd8
TikhonovEugeneA <tih050504@gmail.com>
2025-12-31 15:35:09 +0700

    Добавляет скрипт генерации шаблонов карты

---
+++ Assets/Scripts/Map/MapSpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(TemplatesLoader))]
+public class MapSpawner : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private TemplatesLoader templatesLoader;
+    
+    [Header("Settings")] 
+    [Tooltip("Map parent transform.")]
+    [SerializeField] private Transform templatesParentTransform;
+    
+    [Tooltip("Templates count on scene in one moment.")]
+    [SerializeField] private int templatesPoolSize;
+
+    [Tooltip("Template size.")]
+    [SerializeField] private Vector3 templateSize;
+
+    [Tooltip("Templates on scene.")]
+    [SerializeField] private List<GameObject> spawnedTemplates;
+    
+    private void Awake()
+    {
+        if (this.templatesLoader == null) this.templatesLoader = GetComponent<TemplatesLoader>();
+    }
+
+    private void Update()
+    {
+        if (this.spawnedTemplates.Count < this.templatesPoolSize)
+        {
+            SpawnTemplate();
+        }
+    }
+
+    private void SpawnTemplate()
+    {
+        GameObject template = this.templatesLoader.GetRandomTemplate();
+        GameObject spawnedTemplate = Instantiate(template, this.templatesParentTransform);
+        
+        GameObject lastSpawnedTemplate = this.spawnedTemplates.Last();
+        Vector3 lastSpawnedTemplatePosition = lastSpawnedTemplate.transform.localPosition;
+        Vector3 templatePosition = lastSpawnedTemplatePosition + this.templateSize;
+
+        spawnedTemplate.transform.localPosition = templatePosition;
+        this.spawnedTemplates.Add(spawnedTemplate);
+    }
+    
+    public void DeleteTemplate(GameObject template)
+    {
+        this.spawnedTemplates.Remove(template);
+        Destroy(template);
+    }
+}
