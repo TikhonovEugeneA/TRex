a6544d7b53a873339cf187dc5ef78713f971bc28
TikhonovEugeneA <tih050504@gmail.com>
2025-12-31 15:38:08 +0700

    Модифицирует скрипт загрузки шаблонов

--- Assets/Scripts/ResoursesManagmentSystem/TemplatesLoader.cs
+++ Assets/Scripts/ResoursesManagmentSystem/TemplatesLoader.cs
@@ -6,33 +6,34 @@ using System.Collections.Generic;
 
 public class TemplatesLoader : MonoBehaviour
 {
-    [Header("Settings")]
-
-    [Tooltip("Loaded templates")]
+    [Header("Settings")] 
+    [Tooltip("Loaded templates.")]
     [SerializeField] private List<GameObject> loadedTemplates;
-
-    [Tooltip("Templates folder name in resourses")]
+    
+    [Tooltip("Templates folder name in resources.")]
     [SerializeField] private string templatesFolderName;
 
-    [Tooltip("Templates name prefix")]
-    [SerializeField] private string templatesPrefix;
-
-    [Tooltip("Templates count in resourses folder")]
+    [Tooltip("Template name prefix.")]
+    [SerializeField] private string templatePrefix;
+    
+    [Tooltip("Templates count in resources folder.")]
     [SerializeField] private int templatesCount;
+
     public GameObject GetRandomTemplate()
     {
-        string templateName = templatesFolderName + Random.Range(1, templatesCount);
-
+        int templateId = Random.Range(1, templatesCount);
+        string templateName = templatePrefix + templateId;
         if (loadedTemplates.Exists(t => t.name == templateName))
         {
+            Debug.Log("{<color=cyan><b>Template Loaded Log</b></color>} => [TemplatesLoader] - (<color=yellow>GetRandomTemplate</color>) -> Template " + templateName + " return from loaded templates.");
             GameObject template = loadedTemplates.Find(t => t.name == templateName);
             return template;
         }
 
         string templateResourcePath = Path.Combine(templatesFolderName, templateName);
         GameObject loadedTemplate = Resources.Load<GameObject>(templateResourcePath);
-        
         loadedTemplates.Add(loadedTemplate);
+        Debug.Log("{<color=cyan><b>Template Loaded Log</b></color>} => [TemplatesLoader] - (<color=yellow>GetRandomTemplate</color>) -> Template " + templateName + " loaded from resources.");
         return loadedTemplate;
     }
 }
