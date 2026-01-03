0e42cf4dfee31a83e016209cded3e87949ce18b1
TikhonovEugeneA <tih050504@gmail.com>
2026-01-04 02:38:01 +0700

    Модифицирует скрипт счета очков

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -2,59 +2,65 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ScoreManager : MonoBehaviour
-{
-    [Header("Settings")] 
-    [SerializeField] private int addScorePerIteration;
-    [SerializeField] private float iterationDelta;
-    [SerializeField] private float minIterationDelta;
-    [SerializeField] private float maxIterationDelta;
-    [SerializeField] private float lessDeltaPerSecond;
-    [SerializeField] private float nonLessDeltaTime;
-    [Space] 
-    [SerializeField] private int scoreCount;
-    [SerializeField] private int highScoreCount;
+ public class ScoreManager : MonoBehaviour
+ {
+     [Header("Settings")] 
+     [SerializeField] private int addScorePerIteration;
+     [SerializeField] private float iterationDelta;
+     [SerializeField] private float minIterationDelta;
+     [SerializeField] private float maxIterationDelta;
+     [SerializeField] private float lessDeltaPerSecond;
+     [SerializeField] private float nonLessDeltaTime;
+     [Space] 
+     [SerializeField] private int scoreCount;
+     [SerializeField] private int highScoreCount;
 
-    [SerializeField] private UnityEvent<int> scoreChanged;
-    [SerializeField] private UnityEvent<int> highScoreChanged;
-    
-    private IEnumerator ScoreCounter()
-    {
-        while (true)
-        {
+     [SerializeField] private UnityEvent<int> scoreChanged;
+     [SerializeField] private UnityEvent<int> highScoreChanged;
+     
+     private IEnumerator ScoreCounter()
+     {
+         while (true)
+         {
             scoreCount += addScorePerIteration;
             scoreChanged?.Invoke(scoreCount);
 
-            if (scoreCount > highScoreCount)
-            {
-                highScoreCount = scoreCount;
-                highScoreChanged?.Invoke(highScoreCount);
-            }
+             if (scoreCount > highScoreCount)
+             {
+                 highScoreCount = scoreCount;
+                 highScoreChanged?.Invoke(highScoreCount);
+             }
 
-            yield return new WaitForSeconds(iterationDelta);
-        }
-    }
+             yield return new WaitForSeconds(iterationDelta);
+         }
+     }
 
-    private IEnumerator IterationDeltaCounter()
-    {
-        yield return new WaitForSeconds(nonLessDeltaTime);
-        while (true)
-        {
-            iterationDelta -= lessDeltaPerSecond / 10;
-            iterationDelta = Mathf.Clamp(iterationDelta, minIterationDelta, maxIterationDelta);
+     private IEnumerator IterationDeltaCounter()
+     {
+         yield return new WaitForSeconds(nonLessDeltaTime);
+         while (true)
+         {
+             iterationDelta -= lessDeltaPerSecond / 10;
+             iterationDelta = Mathf.Clamp(iterationDelta, minIterationDelta, maxIterationDelta);
 
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
+             yield return new WaitForSeconds(0.1f);
+         }
+     }
 
-    public void OnGameStart()
-    {
+     public void OnGameStart()
+     {
         StartCoroutine(ScoreCounter());
         StartCoroutine(IterationDeltaCounter());
-    }
-
-    public void OnPlayerDead()
-    {
+     }
+     
+     public void OnPlayerDead()
+     {
         StopAllCoroutines();
-    }
-}
+     }
+
+     public void OnDataLoaded(GameData data)
+     {
+        highScoreCount = data.highScoreCount;
+        highScoreChanged?.Invoke(highScoreCount);
+     }
+ }
